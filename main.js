41f7f22b9649acaec43d0677c00babd844ef2f94
Johnny Briones <jbrrii91@gmail.com>
2020-09-09 23:16:17 -0400

    computerChoice

--- main.js
+++ main.js
@@ -1,51 +1,60 @@
 let userChoice;
 let computerChoice;
+let computerList = ["rock", "paper", "scissor"];
+let choice = computerList[Math.floor(Math.random() * computerList.length)];
 
-let rock = document.getElementById('rock').addEventListener('click',rockPaperScissor);
-let paper = document.getElementById('paper').addEventListener('click',rockPaperScissor);
-let scissor = document.getElementById('scissor').addEventListener('click', rockPaperScissor);
-console.log(rockPaperScissor)
+let rock = document
+  .getElementById("rock")
+  .addEventListener("click", rockPaperScissor);
 
-// computerChoice = 'rock'
-// userChoice = 'rock'
-// console.log(userChoice)
-// console.log(computerChoice)
+let paper = document
+  .getElementById("paper")
+  .addEventListener("click", rockPaperScissor);
 
+let scissor = document
+  .getElementById("scissor")
+  .addEventListener("click", rockPaperScissor);
+  
+userChoice = "rock";
+computerChoice = choice;
 
-function rockPaperScissor(userChoice,computerChoice) {
-    if(userChoice === 'rock' && computerChoice === 'scissor') {
-        document.getElementById('results').innerHTML = 'YOU WIN!';
-    }
-    if (userChoice === 'rock' && computerChoice === 'rock'){
-        document.getElementById('results').innerHTML = 'YOU ARE TIED!';
-    }
-    if (userChoice === 'rock' && computerChoice === 'paper'){
-        document.getElementById('results').innerHTML = 'YOU LOSE!';
-    }
-    if(userChoice === 'paper' && computerChoice === 'rock') {
-        document.getElementById('results').innerHTML = 'YOU WIN!';
-    }
-    if(userChoice === 'paper' && computerChoice === 'paper') {
-        document.getElementById('results').innerHTML = 'YOU ARE TIED!';
-    }
-    if(userChoice === 'paper' && computerChoice === 'scissor') {
-        document.getElementById('results').innerHTML = 'YOU LOSE!';
-    }
-    if(userChoice === 'scissor' && computerChoice === 'paper') {
-        document.getElementById('results').innerHTML = 'YOU WIN!';
-    }
-    
-    if(userChoice === 'scissor' && computerChoice === 'scissor') {
-        document.getElementById('results').innerHTML = 'YOU ARE TIED!';
-    }
-    
-    if(userChoice === 'scissor' && computerChoice === 'rock') {
-        document.getElementById('results').innerHTML = 'YOU LOSE!';
-    }
-    if (userChoice === undefined || computerChoice === undefined) 
-    {document.getElementById('results').innerHTML = 'PICK A WEAPON!'}
+console.log(userChoice);
+console.log(computerChoice);
+
+function rockPaperScissor(userChoice, computerChoice) {
+  if (userChoice === "rock" && computerChoice === "scissor") {
+    document.getElementById("results").innerHTML = "YOU WIN!";
+  }
+  if (userChoice === "rock" && computerChoice === "rock") {
+    document.getElementById("results").innerHTML = "YOU ARE TIED!";
+  }
+  if (userChoice === "rock" && computerChoice === "paper") {
+    document.getElementById("results").innerHTML = "YOU LOSE!";
+  }
+  if (userChoice === "paper" && computerChoice === "rock") {
+    document.getElementById("results").innerHTML = "YOU WIN!";
+  }
+  if (userChoice === "paper" && computerChoice === "paper") {
+    document.getElementById("results").innerHTML = "YOU ARE TIED!";
+  }
+  if (userChoice === "paper" && computerChoice === "scissor") {
+    document.getElementById("results").innerHTML = "YOU LOSE!";
+  }
+  if (userChoice === "scissor" && computerChoice === "paper") {
+    document.getElementById("results").innerHTML = "YOU WIN!";
+  }
+
+  if (userChoice === "scissor" && computerChoice === "scissor") {
+    document.getElementById("results").innerHTML = "YOU ARE TIED!";
+  }
+
+  if (userChoice === "scissor" && computerChoice === "rock") {
+    document.getElementById("results").innerHTML = "YOU LOSE!";
+  }
+  if (userChoice === undefined || computerChoice === undefined) {
+    document.getElementById("results").innerHTML = "PICK A WEAPON!";
+  }
 }
-
 
 rockPaperScissor(userChoice, computerChoice);
 
