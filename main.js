e311d15c0c026c49131376dd056a70d7315bfcdf
Johnny Briones <jbrrii91@gmail.com>
2020-09-11 19:01:42 -0400

    loop

--- main.js
+++ main.js
@@ -5,33 +5,39 @@ let choice = computerList[Math.floor(Math.random() * computerList.length)];
 
 let rock = document
   .getElementById("rock")
-  .addEventListener("click", function(e) {
-    rockPaperScissor(e.target.id,computerList[Math.floor(Math.random() * computerList.length)] );
+  .addEventListener("click", function (e) {
+    rockPaperScissor(
+      e.target.id,
+      computerList[Math.floor(Math.random() * computerList.length)]
+    );
   });
 
 let paper = document
   .getElementById("paper")
-  .addEventListener("click", function(e) {
-    rockPaperScissor(e.target.id, computerList[Math.floor(Math.random() * computerList.length)] );
+  .addEventListener("click", function (e) {
+    rockPaperScissor(
+      e.target.id,
+      computerList[Math.floor(Math.random() * computerList.length)]
+    );
   });
 
 let scissor = document
   .getElementById("scissor")
-  .addEventListener("click", function(e) {
-    rockPaperScissor(e.target.id, computerList[Math.floor(Math.random() * computerList.length)] );
+  .addEventListener("click", function (e) {
+    rockPaperScissor(
+      e.target.id,
+      computerList[Math.floor(Math.random() * computerList.length)]
+    );
   });
 
-// userChoice = "rock";
-// computerChoice = choice;
-  
-// console.log(userChoice);
-// console.log(computerChoice);
-
-
-
 function rockPaperScissor(userChoice, computerChoice) {
-  document.getElementById('computerChooses').innerHTML = computerChoice.toUpperCase();
-  document.getElementById('yourChoice').innerHTML = userChoice.toUpperCase();
+  let score = 0;
+  let results = document.getElementById("results").innerHTML;
+
+  document.getElementById("yourChoice").innerHTML =
+    "You Chose: " + userChoice.toUpperCase();
+  document.getElementById("computerChooses").innerHTML =
+    "The Computer Chose: " + computerChoice.toUpperCase();
 
   if (userChoice === "rock" && computerChoice === "scissor") {
     document.getElementById("results").innerHTML = "YOU WIN!";
@@ -65,14 +71,11 @@ function rockPaperScissor(userChoice, computerChoice) {
   if (userChoice === undefined || computerChoice === undefined) {
     document.getElementById("results").innerHTML = "PICK A WEAPON!";
   }
+  if (results === 'YOU WIN!') {
+    score++;
+    document.getElementById('score').innerHTML = 'Score: ' + score; 
+    console.log(results);
+  }
 }
 
-
-// let computerChooses = document.getElementById('computerChooses').innerHTML = computerChoice;
-
-// rockPaperScissor(userChoice, computerChoice);
-
-// computer randomly picks rock, paper or scissor. Perhaps from list?
-// user picks rock, paper or scissor
-// Winner is decided and displayed
 // Keep track of score
