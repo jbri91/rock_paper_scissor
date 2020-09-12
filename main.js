27a022ee6c57fc434bff6d72f8df49d936719264
repl.it user <replituser@example.com>
2020-09-12 15:10:46 +0000

    change

--- main.js
+++ main.js
@@ -81,9 +81,6 @@ function rockPaperScissor(userChoice, computerChoice) {
     document.getElementById("results").innerHTML = "YOU LOSE!";
     document.getElementById("results").style.color = "red";
   }
-    document.getElementById("score").innerHTML = "Your Score: " + score;
-    console.log(results);
-  }
   if (results === "YOU LOSE!") {
     computerScore++;
     document.getElementById("computerScore").innerHTML =
@@ -102,5 +99,8 @@ function rockPaperScissor(userChoice, computerChoice) {
   if (score === computerScore) {
     document.getElementById("computerScore").style.color = "yellow";
     document.getElementById("score").style.color = "yellow";
+  }
+  document.getElementById("score").innerHTML = "Your Score: " + score;
+    console.log(results);
   }
 }
